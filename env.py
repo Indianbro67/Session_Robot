839fc9a7c589f95404d75b57559705fc26644ec6
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-06-29 15:44:26 +0530

    Update env.py

--- env.py
+++ env.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+API_ID = os.getenv("API_ID", "26670684").strip()
+API_HASH = os.getenv("API_HASH", "60592bded0f25a9633a8133601f2c779").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6157850456:AAECSQKBOrjcmF_mI9ngnltvsHqjV7TBOEk").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "@test94509")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
